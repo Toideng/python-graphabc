8d8dab286beb784d0c9c9a666da04945aee520fe
Toideng <toideng@toideng.com>
2025-03-22 10:00:39 +0000

    Better 03-more-events.py

--- test/03-more-events.py
+++ test/03-more-events.py
@@ -1,23 +1,18 @@
 from graphabc import *
 
-R = 10.
-
-def draw(x, y):
-	global R
-	R *= 1.05
-	SetPenColor(clRandom())
-	SetBrushColor(clRandom())
-	Circle(x, y, int(R))
-
 def click_cb(x, y, mb):
 	if mb != MB_Left:
 		return
-	draw(x, y)
+	SetColor(clRandom())
+	MoveTo(x, y)
+	Circle(x, y, 30)
 
 def move_cb(x, y, mbm):
 	if not mbm & MBM_Left:
 		return
-	draw(x, y)
+	SetColor(clRandom())
+	LineTo(x, y)
+	Circle(x, y, 12)
 
 SetOnMouseDown(click_cb)
 SetOnMouseMove(move_cb)
